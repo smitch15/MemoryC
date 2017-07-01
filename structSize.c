ed464f041a2dc704e42ca6b72378b5e8e928f9ba
Steven Mitchell <smitch15@binghamton.edu>
2017-07-01 17:44:37 -0400

    play with struct padding

--- structSize.c
+++ structSize.c
@@ -6,9 +6,17 @@ int main() {
 		char z;
 		int a;
 		char b;
+		char d;
+		char f;
+		int e;
 	} cStruct;
-	
-	cStruct c = {.z = 'z', .a = 4, .b = 'b'};
-	printf("&cStruct: %p, ccStruct.z: %p, Struct.a: %p, cStruct.b: %p\n", &c, &c.z, &c.a, &c.b);
+	/* char z will be padded with 3 bytes
+	 * int will take up 4 bytes
+	 * chars b and d will take up 2 bytes but then be padded by 2 bytes
+	 * int e will take up 4 bytes
+	 */
+	cStruct c = {.z = 'z', .a = 4, .b = 'b', .d = 'd', .e = 3};
+	printf("&cStruct: %p, ccStruct.z char: %p, Struct.a int: %p, cStruct.b char: %p, cStruct.b char: %p, ", &c, &c.z, &c.a, &c.b, &c.d);
+	printf("ccStruct.f int: %p, Struct.e int: %p\n", &c.f, &c.e);
 	return 0;
 }
